aebf73b4d0b0d7024783a13dc0f301dc13186e44
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-22 15:33:56 +0500

    ch4_L8_self_end Реализована верстка и стиализация Error страницы

--- nuxt-shop/error.vue
+++ nuxt-shop/error.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
+  <div class="container">
     <h1>{{ error.statusCode}} ошибка</h1>
-    <p>{{ error.statusMessage}}</p>
+<!--    <p>{{ error.statusMessage}}</p>-->
+    <p class="error-text">
+      Страница не найдена, попробуйте перейти на главную страницу
+    </p>
     <ActionButton color="ghost">На главную</ActionButton>
   </div>
 </template>
@@ -15,5 +18,19 @@ const {error} = defineProps<{
 </script>
 
 <style scoped>
-
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+  .error-text {
+    color: var(--color-dark-gray);
+    font-size: 20px;
+    margin-bottom: 50px;
+    width:400px;
+    word-wrap: break-word;
+    text-align: center;
+  }
 </style>
