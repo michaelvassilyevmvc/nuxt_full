976d9f98d9b6535d421320b517125a6e2f974ef9
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-23 18:47:29 +0500

    ch5_L7_end Верстка футера завершена

--- nuxt-shop/components/LayoutFooter.vue
+++ nuxt-shop/components/LayoutFooter.vue
@@ -3,44 +3,55 @@
 </script>
 
 <template>
-  <div>
+  <hr>
+  <div class="footer">
     <div class="footer__top">
       <ul>
         <li>
-          <NuxtLink>Контакты</NuxtLink>
+          <NuxtLink to="#">Контакты</NuxtLink>
         </li>
         <li>
-          <NuxtLink>Условия покупки</NuxtLink>
+          <NuxtLink to="#">Условия покупки</NuxtLink>
         </li>
         <li>
-          <NuxtLink>Доставка и возврат</NuxtLink>
+          <NuxtLink to="#">Доставка и возврат</NuxtLink>
         </li>
       </ul>
-      <div>
-        <InputField variant="black"/>
-        <button>-></button>
+      <div class="footer__form">
+        <InputField placeholder="Ваш email для акций и предложений" variant="black"/>
+        <button class="footer__subscribe">
+          <Icon name="icons:arrow-right" size="8px"/>
+        </button>
       </div>
     </div>
     <div class="footer__bottom">
       <div>© {{ new Date().getFullYear() }} Shoppe</div>
-      <div>
-        <NuxtLink>
-          <Icon name="icons:li"/>
+      <div class="footer__social">
+        <NuxtLink to="#">
+          <Icon name="icons:li" size="30px"/>
         </NuxtLink>
-        <NuxtLink>
-          <Icon name="icons:fb"/>
+        <NuxtLink to="#">
+          <Icon name="icons:fb" size="30px"/>
         </NuxtLink>
-        <NuxtLink>
-          <Icon name="icons:in"/>
+        <NuxtLink to="#">
+          <Icon name="icons:in" size="30px"/>
         </NuxtLink>
-        <NuxtLink>
-          <Icon name="icons:tw"/>
+        <NuxtLink to="#">
+          <Icon name="icons:tw" size="30px"/>
         </NuxtLink>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
+.footer {
+  margin: 0 auto;
+  max-width: 1248px;
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+}
+
 .footer__top {
   display: flex;
   justify-content: space-between;
@@ -49,10 +60,52 @@
 .footer__top ul {
   list-style: none;
   display: flex;
+  padding: 0;
+  gap: 30px;
+}
+
+.footer__top ul a {
+  font-size: 16px;
+  font-weight: 400;
+  text-transform: uppercase;
+  color: var(--color-dark-gray);
+  text-decoration: none;
+}
+
+.footer__top ul a:hover {
+  color: var(--color-black);
 }
 
 .footer__bottom {
   display: flex;
   justify-content: space-between;
+}
+
+.footer__social {
+  display: flex;
+  gap: 20px;
+}
+
+.footer__social a {
+  color: var(--color-dark-gray);
+}
+
+.footer__social a:hover {
+  color: var(--color-black);
+}
+
+.footer__form {
+  position: relative;
+}
+
+.footer__form input{
+  min-width: 380px;
+}
+.footer__subscribe {
+  position: absolute;
+  right: 0;
+  top: 10px;
+  background: none;
+  border: none;
 }
 </style>
