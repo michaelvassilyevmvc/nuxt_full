1496985611aa5a4b163f3b6712fb041dc06138c8
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-25 08:50:31 +0500

    ch6_end Работа с серверной и клиентской реализацие компонентов

--- nuxt-shop/nuxt.config.ts
+++ nuxt-shop/nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: {enabled: true},
+  routeRules: {
+    '/about': {prerender: true},
+    '/': {swr: 3600},
+    '/catalog/**': {swr: 3600},
+    '/auth/**': {isr: true}
+  },
   icon: {
     customCollections: [
       {
