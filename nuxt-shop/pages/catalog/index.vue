c18c9c8633840a9a1b95d13a9b73771b490053ba
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-26 09:13:02 +0500

    ch8_L5_end Работа с POST запросом

--- nuxt-shop/pages/catalog/index.vue
+++ nuxt-shop/pages/catalog/index.vue
@@ -1,5 +1,32 @@
+<script setup lang="ts">
+
+import type {GetCategoriesResponse} from "~/interfaces/category.interface";
+
+const config = useRuntimeConfig()
+const API_URL = config.public.apiurl
+const input = ref('')
+
+async function sendData() {
+  const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories",
+      {
+        method: "POST",
+        body:input,
+      })
+  console.log(data)
+}
+
+// try {
+//   const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories")
+//   console.log(data)
+// } catch (error) {
+//   console.error('err',error)
+// }
+
+</script>
 <template>
   <div>
     Catalog
+    <InputField variant="black" v-model="input"/>
+    <button @click="sendData">Отправить</button>
   </div>
 </template>
