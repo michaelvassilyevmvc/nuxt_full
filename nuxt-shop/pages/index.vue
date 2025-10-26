d2432df325b48b4f6044e6ad05696a9fe653a150
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-26 08:59:52 +0500

    ch8_L4_end Получение данных через useFetch

--- nuxt-shop/pages/index.vue
+++ nuxt-shop/pages/index.vue
@@ -2,18 +2,20 @@
 import type {GetCategoriesResponse} from "~/interfaces/category.interface";
 
 const config = useRuntimeConfig()
-try {
-  const API_URL = config.public.apiurl
-  const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories")
-  console.log(data)
-} catch (error) {
-  console.error('err',error)
-}
+const API_URL = config.public.apiurl
+
+const {data, error} = await useFetch<GetCategoriesResponse>(API_URL+"/categories")
+console.log(data.value)
+console.log(error.value)
+
+// try {
+//   const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories")
+//   console.log(data)
+// } catch (error) {
+//   console.error('err',error)
+// }
 </script>
 <template>
   <div>Index
-    <!--    <Icon name="icons:li"/>-->
-    <!--    <NuxtLink to="/about">About</NuxtLink>-->
-    <!--    <InputField variant="black" placeholder="Введите ваш email для подписки"/>-->
   </div>
 </template>
