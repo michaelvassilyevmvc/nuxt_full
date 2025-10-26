ffe8ec025ee2507d974de4bd8eaaae7b1f06d349
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-26 09:46:35 +0500

    ch8_L8_end Использование UseAsyncData

--- nuxt-shop/pages/catalog/index.vue
+++ nuxt-shop/pages/catalog/index.vue
@@ -6,19 +6,19 @@ const config = useRuntimeConfig()
 const API_URL = config.public.apiurl
 const input = ref('')
 
-const {data, error, refresh} = await useFetch<GetCategoriesResponse>(API_URL + "/categories",
-    {
-      method: "POST",
-      body: input,
-      immediate: false,
-      watch: false
-    })
+const {data, error, refresh} = await useAsyncData<GetCategoriesResponse>('categories', () => {
+  return $fetch(API_URL+"/categories")
+})
 console.log(data.value)
 console.log(error.value)
 
 async function sendData() {
   refresh()
 }
+//
+// $fetch - не SSR friendly, используется внутри функции, не на верхнем уровне
+// useFetch - используем только на верхнем, он SSR friendly
+// useAsyncData - SSR friendly, нужен если сложный вариант получения данных
 
 // try {
 //   const data = await $fetch<GetCategoriesResponse>(API_URL + "/categories")
